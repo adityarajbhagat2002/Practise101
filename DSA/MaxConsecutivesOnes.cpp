52eba6e1c3b453adf1b3aed7ccb23e9f0059e9f9
adityarajbhagat2002 <adityabhagat900@gmail.com>
2025-06-11 17:48:12 +0545

    DSA Commit

---
+++ DSA/MaxConsecutivesOnes.cpp
@@ -0,0 +1,27 @@
+https://leetcode.com/problems/max-consecutive-ones/description/
+
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int prevcount =0;
+        int maxcount=0;
+
+
+        for(int i=0; i<nums.size();i++){
+
+            if(nums[i]==1){
+                prevcount++;
+                maxcount=max(prevcount , maxcount);
+            }else{
+             
+                prevcount=0;
+            }
+        }
+
+        return max(maxcount , prevcount );
+
+        
+        
+    }
+};
