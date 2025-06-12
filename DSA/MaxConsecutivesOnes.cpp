23298861a4ec0af16515ef7e658eef4392027c90
adityarajbhagat2002 <adityabhagat900@gmail.com>
2025-06-12 23:57:03 +0545

    DSA Commit

--- DSA/MaxConsecutivesOnes.cpp
+++ DSA/MaxConsecutivesOnes.cpp
@@ -6,19 +6,15 @@ public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
         int prevcount =0;
         int maxcount=0;
-
-
         for(int i=0; i<nums.size();i++){
 
             if(nums[i]==1){
                 prevcount++;
                 maxcount=max(prevcount , maxcount);
             }else{
-             
                 prevcount=0;
             }
         }
-
         return max(maxcount , prevcount );
 
         
